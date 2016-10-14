3ca2ea5e5e4119987ca5b3d49f153107486f670b
aythamit <aythami.games@gmail.com>
2016-10-14 18:40:01 +0100

    a

--- PracticaIA1/src/clases/mapa.java
+++ PracticaIA1/src/clases/mapa.java
@@ -41,41 +41,76 @@ public class mapa
 package clases;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
-public class mapa extends JFrame  
+public class mapa 
 {  
    /** 
       Constructor 
    */  
-	JPanel contenedor;
-	int MAX_M = 5;
-	int MAX_N = 5;
+
+	int MAX_M = 15;
+	int MAX_N = 15;
+	int medida = 32;
+	private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+	Graphics[][] matriz2 = new Graphics[MAX_M][MAX_N];
+	private JButton[][] matriz = new JButton[MAX_M][MAX_N];
+	private JPanel chessBoard;
 
    public mapa()  
    {  
-	   contenedor = new JPanel();
-	  /* contenedor.setLayout(new BoxLayout(contenedor,BoxLayout.Y_AXIS));
-	   
-	   for(int i=0;i<20;i++)
-		   contenedor.add(new JButton("Boton " + (i+1)));*/
-	   
-	   contenedor.setLayout(new GridLayout(MAX_M,MAX_N));
-	   
-	   JButton temp = new JButton("h");
-	   temp.setBackground(Color.CYAN);
-	   for(int i=0;i<(25); i++)
-	   {
-		   contenedor.add(new JButton("boton "+ (i+1)));
-		   //contenedor.add(temp);
-	   }
-	   add(contenedor);
-	   setSize(300,300);
-	   setVisible(true);
-	   
+	   initializeGui();
    }  
+   public final void initializeGui()
+   {
+	   gui.setBorder(new EmptyBorder(2, 2, 2, 2));
+	   
+	   chessBoard = new JPanel(new GridLayout(0, MAX_N));
+	   chessBoard.setBorder(new LineBorder(Color.BLACK));
+	   gui.add(chessBoard);
+	   
+	   Insets buttonMargin = new Insets(0,0,0,0);
+	   
+	   for (int ii = 0; ii < MAX_M; ii++) {
+           for (int jj = 0; jj < MAX_N; jj++) {
+               JButton b = new JButton();
+              // JLabel prueba = new JLabel("p");
+               b.setMargin(buttonMargin);
+               // our chess pieces are 64x64 px in size, so we'll
+               // 'fill this in' using a transparent icon..
+               ImageIcon icon = new ImageIcon(
+                       new BufferedImage(medida, medida, BufferedImage.TYPE_INT_ARGB));
+               b.setIcon(icon);
+               
+               
+               matriz[jj][ii] = b;
+               
+           }
+       }
+	   
+	   for (int ii = 0; ii < MAX_M; ii++) 
+	   {
+           for (int jj = 0; jj < MAX_N; jj++) 
+           {
+        	   chessBoard.add(matriz[jj][ii]);
+           }
+         }
+   }
+   
+   public final JComponent getChessBoard() {
+       return chessBoard;
+   }
 
+   public final JComponent getGui() {
+       return gui;
+   }
+   
+		   
    /** 
       The main method creates an instance of the 
       mapa class, causing it to display 
@@ -84,6 +119,91 @@ public class mapa extends JFrame
 
    public static void main(String[] args)  
    {  
-      new mapa();  
+
+       Runnable r = new Runnable() {
+
+           @Override
+           public void run() {
+               mapa cb =
+                       new mapa();
+
+               JFrame f = new JFrame("ChessChamp");
+               f.add(cb.getGui());
+               f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+               f.setLocationByPlatform(true);
+
+               // ensures the frame is the minimum size it needs to be
+               // in order display the components within it
+               f.pack();
+               // ensures the minimum size is enforced.
+               f.setMinimumSize(f.getSize());
+               f.setVisible(true);
+           }
+       };
+       SwingUtilities.invokeLater(r);
+   
    }  
 } 
+
+
+/*
+ * contenedor = new JPanel();
+	   
+
+	 
+	   contenedor.setLayout(new GridLayout(MAX_M,MAX_N));
+	   int ancho =0;
+	   int alto = 0;
+	   int medida = 20;
+	   
+	   Rectangle2D p = new Rectangle2D.Double(ancho,alto,medida,medida);
+	   
+	   Graphics p2 = getGraphics();
+	   p2.drawRect(ancho,alto,medida,medida);
+		   for(int i=0;i<MAX_M; i++)
+		   {
+			   for(int j=0;j<MAX_N; j++)
+			   {
+				   matriz2[i][j] = p2;
+				   ancho+=medida;
+				   p2.drawRect(ancho,alto,medida,medida);
+				   
+			   }
+			   ancho = 0;
+			   alto += medida;
+			   
+		   }
+
+		   
+	   add(contenedor);
+	   setSize(300,300);
+	   setVisible(true); 
+	   
+	   *
+	   *switch(x)
+	   *case '1'
+	   *{
+	   *	area()
+	   *}break;
+	   *
+	   *case '2'
+	   *{
+	   *	perimetro()
+	   *}break;
+	   *
+	   *case '0'
+	   *{
+	   *	salida = true
+	   *}break;
+	   *
+	   *
+	   *
+	   *
+	   *
+	   *
+	   *
+	   *
+	   *
+	   *
+	   */
+ 
